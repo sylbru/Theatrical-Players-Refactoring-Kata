86b5a5cf9bac55cdfc3ecb936238f9427df30050
sylbru <sylvain.brunerie@gmail.com>
2024-11-06 18:22:02 +0100

    use performance calculator for totalAmount as well

--- php/src/StatementPrinter.php
+++ php/src/StatementPrinter.php
@@ -113,7 +113,7 @@ class StatementPrinter
     {
         return array_reduce(
             $enrichedPerformances,
-            fn ($carry, $item) => $carry + $this->amountFor($item),
+            fn ($carry, $item) => $carry + $this->createPerformanceCalculator($item->toSimplePerformance(), $item->play)->calculateAmount(),
             0,
         );
     }
@@ -139,32 +139,5 @@ class StatementPrinter
     {
         $format = new NumberFormatter('en_US', NumberFormatter::CURRENCY);
         return $format->formatCurrency($amount / 100, 'USD'); // @phpstan-ignore-line
-    }
-
-    private function amountFor(EnrichedPerformance $performance): int
-    {
-        $thisAmount = 0;
-
-        switch ($performance->play->type) {
-            case 'tragedy':
-                $thisAmount = 40000;
-                if ($performance->audience > 30) {
-                    $thisAmount += 1000 * ($performance->audience - 30);
-                }
-                break;
-
-            case 'comedy':
-                $thisAmount = 30000;
-                if ($performance->audience > 20) {
-                    $thisAmount += 10000 + 500 * ($performance->audience - 20);
-                }
-                $thisAmount += 300 * $performance->audience;
-                break;
-
-            default:
-                throw new Error("Unknown type: {$performance->play->type}");
-        }
-
-        return $thisAmount;
     }
 }
